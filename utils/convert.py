dd8f464c5076edb9c197953b00e2189c4621e7b3
Ellen7ions <lzz765177255@gmail.com>
2022-03-14 13:47:35 +0800

    Kernel convert.

--- utils/convert.py
+++ utils/convert.py
@@ -1,6 +1,13 @@
 import cv2
+import numpy as np
 
-def convert_img_coe(img):
+def convert_img_coe():
+    N = 16
+    img = cv2.imread('../img/icon.png')
+    gray = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
+    gray = cv2.resize(gray, (N, N))
+    cv2.imwrite('../img/icon_gray.png', gray)
+    img = gray
     file = open("../tb/img_data.coe", "w");
     file.write("memory_initialization_radix=16;\n")
     file.write("memory_initialization_vector=\n")
@@ -9,12 +16,20 @@ def convert_img_coe(img):
         for j in range(w):
             file.write(f"00{str(hex(img[i][j]))[2:]}\n")
 
+
+def convert_kernel():
+    kernel = np.array([
+        [0, 1, 0],
+        [1, 4, 1],
+        [0, 1, 0],
+    ])
+    kernel = kernel.flatten().tolist()
+    with open('../tb/kernel_weights.txt', "w") as fp:
+        for i in kernel:
+            fp.write(f"{i}\n")
+
 if __name__ == '__main__':
-    N = 16
-    img = cv2.imread('../img/icon.png')
-    gray = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
-    gray = cv2.resize(gray, (N, N))
-    cv2.imwrite('../img/icon_gray.png', gray)
-    convert_img_coe(gray)
+    # convert_img_coe()
+    convert_kernel()
     print("finished!")
     
