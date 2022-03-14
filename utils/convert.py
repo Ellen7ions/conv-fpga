41d76e4bd5317bfd14076fabb9707246e947bf25
Ellen7ions <lzz765177255@gmail.com>
2022-03-14 14:06:00 +0800

    Add convert args.

--- utils/convert.py
+++ utils/convert.py
@@ -1,8 +1,10 @@
 import cv2
+from cv2 import sqrt
 import numpy as np
+import argparse	
 
 def convert_img_coe():
-    N = 16
+    N = 100
     img = cv2.imread('../img/icon.png')
     gray = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
     gray = cv2.resize(gray, (N, N))
@@ -17,19 +19,21 @@ def convert_img_coe():
             file.write(f"00{str(hex(img[i][j]))[2:]}\n")
 
 
-def convert_kernel():
-    kernel = np.array([
-        [0, 1, 0],
-        [1, 4, 1],
-        [0, 1, 0],
-    ])
-    kernel = kernel.flatten().tolist()
+def convert_kernel(kernel):
     with open('../tb/kernel_weights.txt', "w") as fp:
         for i in kernel:
             fp.write(f"{i}\n")
 
 if __name__ == '__main__':
-    # convert_img_coe()
-    convert_kernel()
-    print("finished!")
+    parser = argparse.ArgumentParser()	
+    parser.add_argument('--img', action='store_true', default=False, help='covert a image to gray map')
+    parser.add_argument('--ker', type=int, nargs='+', help='covert a kernel to gray map')
+    args = parser.parse_args()
+    if args.img:
+        convert_img_coe()
+        print("img finished!")
+    elif args.ker:
+        print(args.ker)
+        convert_kernel(args.ker)
+        print("kernel finished!")
     
